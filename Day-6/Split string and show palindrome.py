3533dd4068d145254a2b0a10b0446f6ba50fd119
SayedFazal <syedfazal2956@gmail.com>
2024-08-10 08:49:52 +0530

    Create Split string and show palindrome.py

---
+++ Day-6/Split string and show palindrome.py
@@ -0,0 +1,15 @@
+# write a program to split the strings into substrings in such a way all possible substrings should be a palindrome substring
+def palindromic_splits(s):
+    def is_palindrome(x):
+        return x == x[::-1]
+    def backtrack(start, path):
+        if start == len(s):
+            result.append(path)
+            return
+        for end in range(start + 1, len(s) + 1):
+            if is_palindrome(s[start:end]):
+                backtrack(end, path + [s[start:end]])
+    result = []
+    backtrack(0, [])
+    return result
+print(palindromic_splits("aab"))
