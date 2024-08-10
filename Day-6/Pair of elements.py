0f1fcd69c4ead333f2eca99f7ef8e274380cbb26
SayedFazal <syedfazal2956@gmail.com>
2024-08-10 09:09:55 +0530

    Create Pair of elements.py

---
+++ Day-6/Pair of elements.py
@@ -0,0 +1,7 @@
+#From the given list print the pair of elements adds upto the given target element
+lst = [1, 2, 3, 4, 5, 6]
+target = 7
+for i in range(len(lst)):
+    for j in range(i + 1, len(lst)):
+        if lst[i] + lst[j] == target:
+            print((lst[i], lst[j]))
