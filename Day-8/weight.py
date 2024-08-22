d500f83b8fd5463d0386ac06d95a2ecb4af9e687
SayedFazal <syedfazal2956@gmail.com>
2024-08-22 14:58:19 +0530

    Create weight.py

---
+++ Day-8/weight.py
@@ -0,0 +1,2 @@
+'''You are given an set of items each item conists of weight & values v and you will be given a bag with capacity C. print the items which will gives you more profit when you fill the bag 
+condition: You should exit the bag capacity, you can take or either leave the item'''
