e19757800c8fa3d9434368312469a9cb182466f5
SayedFazal <syedfazal2956@gmail.com>
2024-08-26 13:09:32 +0530

    Update Longest common subsequence.py

--- Day-11/Longest common subsequence.py
+++ Day-11/Longest common subsequence.py
@@ -1,5 +1,6 @@
+#Find longest subsequence in given two string
 def longest_common_subsequence(str1, str2):
-    lcs = [char for char in str1 if char in str2]
+    lcs = [x for x in str1 if x in str2]
     if lcs:
         return ''.join(lcs)
     else:
