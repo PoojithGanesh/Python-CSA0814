9108fca2029a474c88aa191d2341da4b26a09156
SayedFazal <syedfazal2956@gmail.com>
2024-08-27 08:56:48 +0530

    Create Fibonacci sequence.py

---
+++ Day-12/Fibonacci sequence.py
@@ -0,0 +1,8 @@
+#Create a generator function that generates the fibonacci sequence number of elements
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        yield a
+        a, b = b, a + b
+
+print(list(fibonacci(5)))
